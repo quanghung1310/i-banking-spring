7d7b84a4c73ccbb24485e471af5731c2f30041e7
Quang Hung <ngochung.quang.1310@gmail.com>
2020-07-25 22:36:27 +0700

    update

--- src/main/java/com/backend/model/response/UserResponse.java
+++ src/main/java/com/backend/model/response/UserResponse.java
@@ -17,6 +17,7 @@ public class UserResponse {
     private String name;
     private String phone;
     private String createdAt;
+    private String role;
     private List<Account> account;
 
     @Override
