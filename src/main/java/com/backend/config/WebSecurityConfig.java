9acd76b8ed21516ed758156cf14814c678a2c7cc
CLICK <lang.tran>
2020-08-19 22:36:15 +0700

    generate hash transfer

--- src/main/java/com/backend/config/WebSecurityConfig.java
+++ src/main/java/com/backend/config/WebSecurityConfig.java
@@ -34,7 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/authenticate",
-                "/transfer-bank", "/account-bank", "/generate-query-account")
+                "/transfer-bank",
+                "/account-bank",
+                "/generate-query-account",
+                "/generate-transfer")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
