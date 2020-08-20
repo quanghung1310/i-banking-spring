c15669bb0816202c94cd65e77e885252f89c5ffd
Quang Hung <ngochung.quang.1310@gmail.com>
2020-08-20 08:34:02 +0700

    Update timezone

--- src/main/java/com/backend/Application.java
+++ src/main/java/com/backend/Application.java
@@ -4,9 +4,17 @@ import com.backend.config.PartnerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 	private static final String PATH_TO_CONFIG_FOLDER = "conf\\";
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+7:00"));
+	}
 
 	public static void main(String[] args) {
 		PartnerConfig.init(PATH_TO_CONFIG_FOLDER + "partner.json");
